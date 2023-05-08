a34c002751de5961fab79e30e4da494970c3f38a
lbringerl <lbringerl@hotmail.com>
2023-05-09 01:20:53 +0300

    code style

--- src/main.c
+++ src/main.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 
-int loop(){
+int loop()
+{
     int counter = 0;
     int multiplier = 100000000;
-    while(1){
-        if (counter % multiplier == 0){
+    while(1)
+    {
+        if (counter % multiplier == 0)
             printf("(%d)Healthcheck...\n", counter / multiplier);
-        }
         counter += 1;
     }
 }
 
 
-int main(){
+int main()
+{
     loop();
     return 0;
 }
