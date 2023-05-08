95e98151772a731e590d853a15caa09dbdfc6e22
lbringerl <lbringerl@hotmail.com>
2023-05-09 01:19:05 +0300

    check cmake

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int loop(){
+    int counter = 0;
+    int multiplier = 100000000;
+    while(1){
+        if (counter % multiplier == 0){
+            printf("(%d)Healthcheck...\n", counter / multiplier);
+        }
+        counter += 1;
+    }
+}
+
+
+int main(){
+    loop();
+    return 0;
+}
