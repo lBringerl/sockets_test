76efc2d7712c18154bbcd701eca999e7260aba7e
lbringerl <lbringerl@hotmail.com>
2023-05-09 23:40:39 +0300

    use pthreads

--- src/main.c
+++ src/main.c
@@ -1,13 +1,23 @@
+#include <pthread.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <time.h>
 
 
-int loop()
+struct loop_args
 {
+    bool *exit_flag_ptr;
+};
+
+
+int loop(struct loop_args *args)
+{
+    bool *exit_flag = args->exit_flag_ptr;
     time_t time_beg = time(NULL);
     time_t delay = 5;
     while(1)
     {
+        if(*exit_flag) break;
         time_t time_cur = time(NULL);
         time_t diff = time_cur - time_beg;
         printf("(%d)Healthcheck...\n", diff);
@@ -16,8 +26,15 @@ int loop()
 }
 
 
+
 int main()
 {
-    loop();
+    bool exit_flag = false;
+    pthread_t thread_id;
+    pthread_t thread_id2;
+    struct loop_args args;
+    args.exit_flag_ptr = &exit_flag;
+    pthread_create(&thread_id, NULL, (void *)&loop, (void *)&args);
+    pthread_join(thread_id, NULL);
     return 0;
 }
