56b7aa618593755b24a7c997ab6e78d0c0ba3fb7
lbringerl <lbringerl@hotmail.com>
2023-05-09 21:56:34 +0300

    use sleep function

--- src/main.c
+++ src/main.c
@@ -6,16 +6,12 @@ int loop()
 {
     time_t time_beg = time(NULL);
     time_t delay = 5;
-    time_t prev_diff = -1;
     while(1)
     {
         time_t time_cur = time(NULL);
         time_t diff = time_cur - time_beg;
-        if (diff != prev_diff && diff % delay == 0)
-        {
-            printf("(%d)Healthcheck...\n", diff);
-            prev_diff = diff;
-        }
+        printf("(%d)Healthcheck...\n", diff);
+        sleep(delay);
     }
 }
 
