09425103f3c46bc9077d7ace4b844cd19eec7600
lbringerl <lbringerl@hotmail.com>
2023-05-09 16:10:33 +0300

    use time

--- src/main.c
+++ src/main.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+#include <time.h>
 
 
 int loop()
 {
-    int counter = 0;
-    int multiplier = 100000000;
+    time_t time_beg = time(NULL);
+    time_t delay = 5;
+    time_t prev_diff = -1;
     while(1)
     {
-        if (counter % multiplier == 0)
-            printf("(%d)Healthcheck...\n", counter / multiplier);
-        counter += 1;
+        time_t time_cur = time(NULL);
+        time_t diff = time_cur - time_beg;
+        if (diff != prev_diff && diff % delay == 0)
+        {
+            printf("(%d)Healthcheck...\n", diff);
+            prev_diff = diff;
+        }
     }
 }
 
