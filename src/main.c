cde864a1c0130a171a7a6a0cbb9296b3df0e6a7b
lbringerl <lbringerl@hotmail.com>
2023-05-20 17:29:59 +0300

    async healthcheck

--- src/main.c
+++ src/main.c
@@ -1,40 +1,58 @@
 #include <pthread.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 
 
-struct loop_args
+struct loop_state
 {
-    bool *exit_flag_ptr;
+    bool exit_flag;
 };
 
 
-int loop(struct loop_args *args)
+int loop(struct loop_state *state)
 {
-    bool *exit_flag = args->exit_flag_ptr;
     time_t time_beg = time(NULL);
     time_t delay = 5;
-    while(1)
+    time_t prev_printed = -1;
+    while(!state->exit_flag)
     {
-        if(*exit_flag) break;
         time_t time_cur = time(NULL);
         time_t diff = time_cur - time_beg;
-        printf("(%d)Healthcheck...\n", diff);
-        sleep(delay);
+        if (diff % delay == 0 && time_cur != prev_printed)
+        {
+            printf("(%d)Healthcheck...\n", diff);
+            prev_printed = time_cur;
+        }
     }
 }
 
 
+void ask_exit()
+{
+    char input_str[10];
+    char exit_str[] = "EXT";
+    while(1)
+    {
+        printf("Enter '%s' to exit.\n", exit_str);
+        scanf("%s", input_str);
+        if (!strcmp(input_str, exit_str))
+            break;
+        else
+            printf("Unknown input. Try again\n");
+    }
+}
+
 
 int main()
 {
-    bool exit_flag = false;
-    pthread_t thread_id;
-    pthread_t thread_id2;
-    struct loop_args args;
-    args.exit_flag_ptr = &exit_flag;
-    pthread_create(&thread_id, NULL, (void *)&loop, (void *)&args);
-    pthread_join(thread_id, NULL);
+    pthread_t loop_thread_id;
+    struct loop_state state;
+    state.exit_flag = false;
+    pthread_create(&loop_thread_id, NULL, (void *)&loop, (void *)&state);
+    ask_exit();
+    state.exit_flag = true;
+    pthread_join(loop_thread_id, NULL);
     return 0;
 }
